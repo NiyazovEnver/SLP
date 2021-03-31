540e3cb31f1eb2e633aeda4907082cd512904d9d
NiyazovEnver <71487196+NiyazovEnver@users.noreply.github.com>
2021-04-01 00:37:01 +0300

    Update Niyazov.lisp

--- Niyazov.lisp
+++ Niyazov.lisp
@@ -130,4 +130,22 @@
 (print (_set-difference '(1 2 3 4 5) '(4 5 6 7)))
 (print (_set-difference nil '(1 2 3)))
 
+;Задача №43
+;Определите функцию, подсчитывающую количество всех вершин данного дерева заданной высоты
+;(defun tree-count (tree lv)
+;  (cond 
+;      ((null tree) 0)
+;      ((equal lv 0) 1)
+;      (t (+ (tree-count (car tree) (- lv 1)) (tree-count (caddr tree) (- lv 1))))
+;  )
+;)
+ 
+ 
+;(print (tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 1))
+ 
+;(tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 2)
+ 
+;(tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 3)
+ 
+
 
