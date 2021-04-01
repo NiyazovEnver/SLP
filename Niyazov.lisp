dfd51ea394167aafea6221cfaabd516ca12e7bd9
NiyazovEnver <71487196+NiyazovEnver@users.noreply.github.com>
2021-04-01 08:58:52 +0300

    Update Niyazov.lisp

--- Niyazov.lisp
+++ Niyazov.lisp
@@ -28,8 +28,10 @@
      )
 )
 
+(print "Задача №5. Определите функцию,которая увеличивает элементы исходного списка на единицу")
 (print (increase '(1 2 3)))
 (print (increase '((1 (2 3)) ((4 (5) 6)) ((7 8) 9))))
+(print (increase nil))
 
 ;Задача №12
 ;Определите функцию, заменяющую в исходном списке два подряд идущих одинаковых элемента одним.
@@ -44,6 +46,7 @@
      )
 )
 
+(print ";Задача №12 Определите функцию, заменяющую в исходном списке два подряд идущих одинаковых элемента одним.")
 (print (mrg '(1 1 1 2 2 2 3 3 3)))
 (print (mrg '(a b a b a a b b a)))
 (print (mrg '((a b) (a b) a b a a b b a)))
@@ -66,7 +69,7 @@
     )
 )
 
-
+(print ";Задача №13. Определите функцию, удаляющую в исходном списке все повторные вхождеению элементов.")
 (print (double '(a b b a a)))
 (print (double '(123 css sscc css css a a)))
 (print (double '(a (b c) 1 1 3 6)))
@@ -77,6 +80,8 @@
 
 (defun SWAP-TWO-ELEMENT (L N1 N2)
     (cond
+        ((equal N1 N2) L)
+        ((> N1 N2) (SWAP-TWO-ELEMENT L N2 N1))
         ((> N1 0) 
             (cons (car L) (SWAP-TWO-ELEMENT (cdr L) (- N1 1) (- N2 1))))
         ((> N2 1)
@@ -86,12 +91,17 @@
              (SWAP-TWO-ELEMENT (cons (car L) (cddr L)) (- N1 1) (- N2 1))
             )
         )
+        
         (T 
             (cons (cadr L) (cons (car L) (cddr L))))
     )
 )
 
+(print "Задача №14. Определите функцию,осуществляющую перестановку двух элементов списка с заданными номерами.")
 (print (SWAP-TWO-ELEMENT '(1 2 3) 1 2))
+(print (SWAP-TWO-ELEMENT '(1 2 (4 5 6)) 1 2))
+(print (SWAP-TWO-ELEMENT '(1 2 3) 2 0))
+(print (SWAP-TWO-ELEMENT '(1 2 3) 2 2))
 
 ;Задача №22
 ;Определите функцию,которая обращает список(аbс) и разбивает его на уровни (((с)b)а)
@@ -106,6 +116,7 @@
     )
 )
 
+(print ";Задача №22. Определите функцию,которая обращает список(аbс) и разбивает его на уровни (((с)b)а)")
 (print (F '(a b c)))
 (print (F '((a b c) b c)))
 (print (F '(nil b c)))
@@ -127,6 +138,7 @@
      )
 )
 
+(print "Задача №37. Определите функцию ПЕРЕСЕЧЕНИЕ, формирующую пересечение двух множеств, т.е. множество из их общих элементов")
 (print (_intersection '(a b c) '(b c d)))
 (print (_intersection '(a ) '( d) ))
 (print (_intersection '(a ) 'nil ))
@@ -145,26 +157,27 @@
     )
 )
 
+(print ";Задача №40 Определите функцию РАЗНОСТЬ, формирующую разность двух множеств, т.е. удаляющую из первого множества все общие со вторым множеством элементы.")
 (print (_set-difference '(4 5 6 7) '(4 5 6 7)))
 (print (_set-difference '(1 2 3 4 5) '(4 5 6 7)))
 (print (_set-difference nil '(1 2 3)))
 
 ;Задача №43
 ;Определите функцию, подсчитывающую количество всех вершин данного дерева заданной высоты
-;(defun tree-count (tree lv)
-;  (cond 
-;      ((null tree) 0)
-;      ((equal lv 0) 1)
-;      (t (+ (tree-count (car tree) (- lv 1)) (tree-count (caddr tree) (- lv 1))))
-;  )
-;)
+(defun tree-count (tree lv)
+  (cond 
+      ((null tree) 0)
+      ((equal lv 0) 1)
+      (t (+ (tree-count (car tree) (- lv 1)) (tree-count (caddr tree) (- lv 1))))
+  )
+)
  
+(print "Задача №43 Определите функцию, подсчитывающую количество всех вершин данного дерева заданной высоты")
+(print (tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 1))
  
-;(print (tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 1))
+(print (tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 2))
  
-;(tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 2)
- 
-;(tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 3)
+(print (tree-count '(((nil 2 nil) 6 (nil -8 nil)) 7 (nil 12 nil)) 3))
  
 
 
